c2bbd7c8cb418a57363487f2cacb0bda1f2d5afa
Elshan <e.kodikara@optergy.com>
2020-06-22 15:05:52 +1000

    adding logs to postinst

--- default.sh
+++ default.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+1>&2 echo "executing default.sh"
 chmod +x ./profiles/DEBELS.postinst
 
 #d-i preseed/late_command string in-target "sed -i 's/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"net.ifnames=0 biosdevname=0\"/' /etc/default/grub"
