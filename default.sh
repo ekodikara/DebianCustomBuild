75688d5eaf586486053218b9928a75922d83483e
Elshan <e.kodikara@optergy.com>
2020-06-22 15:02:28 +1000

    changed network configurations

--- default.sh
+++ default.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+chmod +x ./profiles/DEBELS.postinst
+
 #d-i preseed/late_command string in-target "sed -i 's/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"net.ifnames=0 biosdevname=0\"/' /etc/default/grub"
+
 #d-i preseed/late_command string in-target "update-grub"
+
 build-simple-cdd --conf default.conf
