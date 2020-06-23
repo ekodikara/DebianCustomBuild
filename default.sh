e08a5c5f9e8adf03aa2d9764a47c761376fd6ac5
Elshan <e.kodikara@optergy.com>
2020-06-24 02:00:17 +1000

    added changes for debs/ preseeds

--- default.sh
+++ default.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-1>&2 echo "executing default.sh"
-chmod +x ./profiles/DEBELS.postinst
+echo "removing old debs"
+rm -rf tmp/
 
+echo "setting executables"
+chmod +x ./profiles/default.postinst
+chmod +x ./profiles/DEBELS.postinst
+chmod +x ./default.postinst
+echo "converting files"
+dos2unix ./default.postinst ./profiles/*.*
 #d-i preseed/late_command string in-target "sed -i 's/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"net.ifnames=0 biosdevname=0\"/' /etc/default/grub"
 
 #d-i preseed/late_command string in-target "update-grub"
