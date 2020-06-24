4ce7e2e0739c1ed969df846fb43b503044bde35f
Elshan <e.kodikara@optergy.com>
2020-06-24 14:07:15 +1000

    remove some files from converting.

--- default.sh
+++ default.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "removing old debs"
-rm -rf tmp/
+#rm -rf tmp/
 
 echo "setting executables"
-chmod +x ./profiles/default.postinst
+#chmod +x ./profiles/default.postinst
 chmod +x ./profiles/DEBELS.postinst
-chmod +x ./default.postinst
+#chmod +x ./default.postinst
 echo "converting files"
 dos2unix ./default.postinst ./profiles/*.*
 #d-i preseed/late_command string in-target "sed -i 's/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"net.ifnames=0 biosdevname=0\"/' /etc/default/grub"
