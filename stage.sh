6c4bdfc433ed84a48a344eb8404a24debf914078
Elshan <e.kodikara@optergy.com>
2020-06-25 12:28:59 +1000

    Added stage profiles

--- stage.sh
+++ stage.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "removing old debs"
-#rm -rf tmp/
+rm -rf tmp/
 
 echo "setting executables"
 #chmod +x ./profiles/default.postinst
 chmod +x ./profiles/stage.postinst
 #chmod +x ./default.postinst
 echo "converting files"
-dos2unix ./profiles/*.*
+#dos2unix ./profiles/*.*
 #d-i preseed/late_command string in-target "sed -i 's/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"net.ifnames=0 biosdevname=0\"/' /etc/default/grub"
 
 #d-i preseed/late_command string in-target "update-grub"
