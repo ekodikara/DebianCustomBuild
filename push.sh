128a5c9c5711c234b5aaff7675f97dca164335ac
Elshan <e.kodikara@optergy.com>
2020-07-03 11:24:56 +1000

    Success full build

--- push.sh
+++ push.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sshpass -p elshan scp -r *.conf *.sh *.txt profiles/ optergy@192.168.3.88:/home/optergy/deb-optergy/ 
+sshpass -p elshan rsync -chavzP --stats *.conf *.sh *.txt  optergy@192.168.3.88:/home/optergy/deb-optergy/
+sshpass -p elshan scp -r profiles/ optergy@192.168.3.88:/home/optergy/deb-optergy/profiles/ 
+sshpass -p elshan ssh optergy@192.168.3.88 chmod +x "/home/optergy/deb-optergy/stage.sh"
