e9c29c9e86336ea65caa21185a5ff050de552bba
Elshan <e.kodikara@optergy.com>
2020-07-12 14:19:02 +1000

    delete copy images and default configs

--- copy-image.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#sshpass -p elshan scp optergy@192.168.3.88:/home/optergy/deb-optergy/images/debian-9.12-amd64-CD-1.iso /Users/elshankodikara/Dev/optergy/Debian9/Images/ 
-sshpass -p elshan rsync -chavzP --stats optergy@192.168.3.88:/home/optergy/deb-optergy/images/debian-9.12-amd64-CD-1.iso ~/Dev/optergy/Debian9/Images/
-#ls -lah /Users/elshankodikara/Dev/optergy/Debian9/Images/
-
